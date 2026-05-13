b86b9181afc7f6a573f7a9d67cf864770e7a36fc
Martin Espinosa <felipe.espinosa@udla.edu.ec>
2026-05-13 15:05:58 -0500

    ejercicio 2 aumento de aux

--- segundo_ejercicio.c
+++ segundo_ejercicio.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
 int main(){
     char vocales[]={'a','e','i','o','u'};
-    char *ptr;
+    
+    char *ptr, *aux;
     ptr=&vocales[0];
+    aux=&vocales[4];
+    *aux='x';
     
     for (int i=0;i<5;i++){
-        printf("vocales[%d]%c\n",i,vocales[i]);
+        printf("vocales[%d]%c ",i,vocales[i]);
         printf("%c\n",*(ptr++));
     }
 }
