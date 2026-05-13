87556be3333d334263949c711d304aaf0f436650
Martin Espinosa <felipe.espinosa@udla.edu.ec>
2026-05-13 14:45:45 -0500

    ejercicio 2 ducplicndo el vector de vocales

--- segundo_ejercicio.c
+++ segundo_ejercicio.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 int main(){
     char vocales[]={'a','e','i','o','u'};
+    char *ptr;
+    ptr=&vocales[0];
+    
     for (int i=0;i<5;i++){
         printf("vocales[%d]%c\n",i,vocales[i]);
+        printf("%c\n",*(ptr++));
     }
 }
